3bbea27992ff34b0252f2fb521711a70f64998f3
Trey Hunner <trey@treyhunner.com>
2013-05-27 12:08:20 -0700

    Raise exception when package not hosted on PyPI

--- pep438.py
+++ pep438.py
@@ -15,6 +15,7 @@ from reqfileparser import parse
 def get_links(package_name):
     """Return list of links on package's PyPI page"""
     response = requests.get('https://pypi.python.org/simple/%s' % package_name)
+    response.raise_for_status()
     page = lxml.html.fromstring(response.content)
     external_links = [link for link in page.xpath('//a')
                       if not link.get('href').startswith('../')]
